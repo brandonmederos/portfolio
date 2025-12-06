43cc1112ebae10544d09ed8ad469bfaf6e4be726
brandonmederos <65429510+brandonmederos@users.noreply.github.com>
2025-12-05 20:52:59 -0500

    cleared some bloat, and made run on local machine

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,8 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
+  site: "https://brandonmederos.com",
+  base: "/",
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),

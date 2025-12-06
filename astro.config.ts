5480d0f4ff4eb120b7cc7d9f73845d5124957133
brandonmederos <65429510+brandonmederos@users.noreply.github.com>
2025-12-05 21:38:50 -0500

    rm http:// changed https://

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,7 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://wwwbrandonmederos.com",
+  site: "https://wwwbrandonmederos.com",
   base: "/",
   integrations: [
     sitemap({

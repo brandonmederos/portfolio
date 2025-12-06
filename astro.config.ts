69aac196d9abc2fe6ae3def3f13285f1ac140016
brandonmederos <65429510+brandonmederos@users.noreply.github.com>
2025-12-05 21:32:06 -0500

    added www to site astro

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,7 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://brandonmederos.com",
+  site: "https://wwwbrandonmederos.com",
   base: "/",
   integrations: [
     sitemap({
